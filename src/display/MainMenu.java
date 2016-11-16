c84c78114e2b6d6e8d051f4472ffc6f35379da68
Michael Brown <mike_brown@rogers.com>
2016-11-16 09:05:48 -0500

    file location fix

--- src/display/MainMenu.java
+++ src/display/MainMenu.java
@@ -52,19 +52,10 @@ public class MainMenu extends JFrame {
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[] {0, 0, 0};
+		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
 		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{0.0, 1.0, 1.0};
+		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
-		
-		JToolBar toolBar = new JToolBar();
-		GridBagConstraints gbc_toolBar = new GridBagConstraints();
-		gbc_toolBar.anchor = GridBagConstraints.WEST;
-		gbc_toolBar.gridwidth = 2;
-		gbc_toolBar.insets = new Insets(0, 0, 5, 5);
-		gbc_toolBar.gridx = 0;
-		gbc_toolBar.gridy = 0;
-		contentPane.add(toolBar, gbc_toolBar);
 		
 		JPanel GlobalContainer = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) GlobalContainer.getLayout();
@@ -72,7 +63,7 @@ public class MainMenu extends JFrame {
 		gbc_GlobalContainer.insets = new Insets(0, 0, 5, 5);
 		gbc_GlobalContainer.fill = GridBagConstraints.BOTH;
 		gbc_GlobalContainer.gridx = 0;
-		gbc_GlobalContainer.gridy = 1;
+		gbc_GlobalContainer.gridy = 0;
 		contentPane.add(GlobalContainer, gbc_GlobalContainer);
 		
 		JButton saveDatabaseButton = new JButton("Save");
@@ -89,23 +80,24 @@ public class MainMenu extends JFrame {
 		gbc_ActionContainer.insets = new Insets(0, 0, 5, 0);
 		gbc_ActionContainer.fill = GridBagConstraints.BOTH;
 		gbc_ActionContainer.gridx = 1;
-		gbc_ActionContainer.gridy = 1;
+		gbc_ActionContainer.gridy = 0;
 		contentPane.add(ActionContainer, gbc_ActionContainer);
 		
 		JPanel ViewContainer = new JPanel();
 		GridBagConstraints gbc_ViewContainer = new GridBagConstraints();
-		gbc_ViewContainer.insets = new Insets(0, 0, 0, 5);
+		gbc_ViewContainer.insets = new Insets(0, 0, 5, 5);
 		gbc_ViewContainer.fill = GridBagConstraints.BOTH;
 		gbc_ViewContainer.gridx = 0;
-		gbc_ViewContainer.gridy = 2;
+		gbc_ViewContainer.gridy = 1;
 		contentPane.add(ViewContainer, gbc_ViewContainer);
 		ViewContainer.setLayout(new BorderLayout(0, 0));
 		
 		JPanel ListContainer = new JPanel();
 		GridBagConstraints gbc_ListContainer = new GridBagConstraints();
+		gbc_ListContainer.insets = new Insets(0, 0, 5, 0);
 		gbc_ListContainer.fill = GridBagConstraints.BOTH;
 		gbc_ListContainer.gridx = 1;
-		gbc_ListContainer.gridy = 2;
+		gbc_ListContainer.gridy = 1;
 		contentPane.add(ListContainer, gbc_ListContainer);
 	}
 
