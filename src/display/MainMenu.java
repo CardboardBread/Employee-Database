7e859990f385df4a21a808959c1ab7e4c6edfefb
Michael Brown <mike_brown@rogers.com>
2016-11-16 09:41:00 -0500

    control panel update

--- src/display/MainMenu.java
+++ src/display/MainMenu.java
@@ -20,10 +20,15 @@ import javax.swing.JTextPane;
 import javax.swing.JSplitPane;
 import javax.swing.JToolBar;
 import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EtchedBorder;
 
 public class MainMenu extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField searchField;
 
 	/**
 	 * Launch the application.
@@ -46,42 +51,135 @@ public class MainMenu extends JFrame {
 	 */
 	public MainMenu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 710, 505);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(3, 3, 3, 3));
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWidths = new int[]{0, 0, 0};
-		gbl_contentPane.rowHeights = new int[]{0, 0, 0, 0, 0};
-		gbl_contentPane.columnWeights = new double[]{1.0, 1.0, Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_contentPane.columnWidths = new int[] {0, 0};
+		gbl_contentPane.rowHeights = new int[] {0, 0};
+		gbl_contentPane.columnWeights = new double[]{0.0, 1.0};
+		gbl_contentPane.rowWeights = new double[]{0.0, 1.0};
 		contentPane.setLayout(gbl_contentPane);
 		
 		JPanel GlobalContainer = new JPanel();
-		FlowLayout flowLayout = (FlowLayout) GlobalContainer.getLayout();
+		GlobalContainer.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		GridBagConstraints gbc_GlobalContainer = new GridBagConstraints();
 		gbc_GlobalContainer.insets = new Insets(0, 0, 5, 5);
 		gbc_GlobalContainer.fill = GridBagConstraints.BOTH;
 		gbc_GlobalContainer.gridx = 0;
 		gbc_GlobalContainer.gridy = 0;
 		contentPane.add(GlobalContainer, gbc_GlobalContainer);
+		GridBagLayout gbl_GlobalContainer = new GridBagLayout();
+		gbl_GlobalContainer.columnWidths = new int[] {53, 57, 55};
+		gbl_GlobalContainer.rowHeights = new int[] {0, 0};
+		gbl_GlobalContainer.columnWeights = new double[]{0.0, 1.0, 0.0};
+		gbl_GlobalContainer.rowWeights = new double[]{0.0, 0.0};
+		GlobalContainer.setLayout(gbl_GlobalContainer);
+		
+		JButton newButton = new JButton("New");
+		GridBagConstraints gbc_newButton = new GridBagConstraints();
+		gbc_newButton.fill = GridBagConstraints.BOTH;
+		gbc_newButton.insets = new Insets(0, 3, 0, 5);
+		gbc_newButton.gridx = 0;
+		gbc_newButton.gridy = 1;
+		GlobalContainer.add(newButton, gbc_newButton);
 		
 		JButton saveDatabaseButton = new JButton("Save");
-		GlobalContainer.add(saveDatabaseButton);
+		GridBagConstraints gbc_saveDatabaseButton = new GridBagConstraints();
+		gbc_saveDatabaseButton.fill = GridBagConstraints.BOTH;
+		gbc_saveDatabaseButton.insets = new Insets(0, 3, 0, 5);
+		gbc_saveDatabaseButton.gridx = 1;
+		gbc_saveDatabaseButton.gridy = 1;
+		GlobalContainer.add(saveDatabaseButton, gbc_saveDatabaseButton);
 		
 		JButton loadDatabaseButton = new JButton("Load");
-		GlobalContainer.add(loadDatabaseButton);
+		GridBagConstraints gbc_loadDatabaseButton = new GridBagConstraints();
+		gbc_loadDatabaseButton.fill = GridBagConstraints.BOTH;
+		gbc_loadDatabaseButton.insets = new Insets(0, 3, 0, 5);
+		gbc_loadDatabaseButton.gridx = 2;
+		gbc_loadDatabaseButton.gridy = 1;
+		GlobalContainer.add(loadDatabaseButton, gbc_loadDatabaseButton);
 		
-		JButton exitButton = new JButton("Exit");
-		GlobalContainer.add(exitButton);
+		JLabel globalControlLabel = new JLabel("Database Controls");
+		GridBagConstraints gbc_globalControlLabel = new GridBagConstraints();
+		gbc_globalControlLabel.gridwidth = 3;
+		gbc_globalControlLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_globalControlLabel.gridx = 0;
+		gbc_globalControlLabel.gridy = 0;
+		GlobalContainer.add(globalControlLabel, gbc_globalControlLabel);
 		
 		JPanel ActionContainer = new JPanel();
+		ActionContainer.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		GridBagConstraints gbc_ActionContainer = new GridBagConstraints();
 		gbc_ActionContainer.insets = new Insets(0, 0, 5, 0);
 		gbc_ActionContainer.fill = GridBagConstraints.BOTH;
 		gbc_ActionContainer.gridx = 1;
 		gbc_ActionContainer.gridy = 0;
 		contentPane.add(ActionContainer, gbc_ActionContainer);
+		GridBagLayout gbl_ActionContainer = new GridBagLayout();
+		gbl_ActionContainer.columnWidths = new int[] {40, 51, 63, 51, 10, 33, 86};
+		gbl_ActionContainer.rowHeights = new int[] {30, 0};
+		gbl_ActionContainer.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		gbl_ActionContainer.rowWeights = new double[]{0.0, 0.0};
+		ActionContainer.setLayout(gbl_ActionContainer);
+		
+		JLabel employeeControlLabel = new JLabel("Employee Controls");
+		GridBagConstraints gbc_employeeControlLabel = new GridBagConstraints();
+		gbc_employeeControlLabel.gridwidth = 7;
+		gbc_employeeControlLabel.insets = new Insets(5, 5, 5, 5);
+		gbc_employeeControlLabel.gridx = 0;
+		gbc_employeeControlLabel.gridy = 0;
+		ActionContainer.add(employeeControlLabel, gbc_employeeControlLabel);
+		
+		JButton addButton = new JButton("Add");
+		GridBagConstraints gbc_addButton = new GridBagConstraints();
+		gbc_addButton.insets = new Insets(0, 3, 0, 5);
+		gbc_addButton.fill = GridBagConstraints.BOTH;
+		gbc_addButton.gridx = 0;
+		gbc_addButton.gridy = 1;
+		ActionContainer.add(addButton, gbc_addButton);
+		
+		JButton deleteButton = new JButton("Delete");
+		GridBagConstraints gbc_deleteButton = new GridBagConstraints();
+		gbc_deleteButton.fill = GridBagConstraints.BOTH;
+		gbc_deleteButton.insets = new Insets(0, 3, 0, 5);
+		gbc_deleteButton.gridx = 1;
+		gbc_deleteButton.gridy = 1;
+		ActionContainer.add(deleteButton, gbc_deleteButton);
+		
+		JButton editButton = new JButton("Edit");
+		GridBagConstraints gbc_editButton = new GridBagConstraints();
+		gbc_editButton.fill = GridBagConstraints.BOTH;
+		gbc_editButton.insets = new Insets(0, 3, 0, 5);
+		gbc_editButton.gridx = 2;
+		gbc_editButton.gridy = 1;
+		ActionContainer.add(editButton, gbc_editButton);
+		
+		JButton replaceButton = new JButton("Replace");
+		GridBagConstraints gbc_replaceButton = new GridBagConstraints();
+		gbc_replaceButton.fill = GridBagConstraints.BOTH;
+		gbc_replaceButton.insets = new Insets(0, 3, 0, 5);
+		gbc_replaceButton.gridx = 3;
+		gbc_replaceButton.gridy = 1;
+		ActionContainer.add(replaceButton, gbc_replaceButton);
+		
+		JLabel searchLabel = new JLabel("Search");
+		GridBagConstraints gbc_searchLabel = new GridBagConstraints();
+		gbc_searchLabel.fill = GridBagConstraints.BOTH;
+		gbc_searchLabel.insets = new Insets(0, 3, 0, 5);
+		gbc_searchLabel.gridx = 5;
+		gbc_searchLabel.gridy = 1;
+		ActionContainer.add(searchLabel, gbc_searchLabel);
+		
+		searchField = new JTextField();
+		GridBagConstraints gbc_searchField = new GridBagConstraints();
+		gbc_searchField.insets = new Insets(0, 3, 0, 5);
+		gbc_searchField.fill = GridBagConstraints.BOTH;
+		gbc_searchField.gridx = 6;
+		gbc_searchField.gridy = 1;
+		ActionContainer.add(searchField, gbc_searchField);
+		searchField.setColumns(10);
 		
 		JPanel ViewContainer = new JPanel();
 		GridBagConstraints gbc_ViewContainer = new GridBagConstraints();
