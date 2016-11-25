2c4ae9e9efedc08ef6d5babb7e74239a294db192
Rylan Crooks <Rylan Crooks>
2016-11-25 09:25:49 -0500

    cancer the adventures of syncro merge

--- src/database/FileLoader.java
+++ src/database/FileLoader.java
@@ -4,40 +4,43 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class FileLoader {
 
-	private FileReader fileReader;
-	private BufferedReader bufferedReader;
-	private String line;
+	
 
-	public FileLoader(String fileName) {
-
-		try {
-			fileReader = new FileReader(fileName);
-			bufferedReader = new BufferedReader(fileReader);
-		} catch (FileNotFoundException ex) {
-			System.out.println("Unable to open file '" + fileName + "'");
-		} catch (@SuppressWarnings("hiding") IOException ex) {
-			ex.printStackTrace();
-		}
-
-	}
-
-	public ArrayList<String> readFile() {
+	public ArrayList<String> readFile(String fileName) {
+		
 		ArrayList<String> fileLoad = new ArrayList<String>();
+		String line;
+		
 		try {
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			while ((line = bufferedReader.readLine()) != null) {
 				fileLoad.add(line);
 			}
-		} catch (NumberFormatException e) {
-			System.out.println("BufferedReader could not be read");
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("BufferedReader could not be read");
-			e.printStackTrace();
+			bufferedReader.close();
+		} catch (FileNotFoundException ex) {
+			System.out.println("Unable to open file '" + fileName + "'");
+		} catch (IOException ex) {
+			ex.printStackTrace();
 		}
+
 		return fileLoad;
+	}
+
+	public void saveFile(ArrayList<String> load, String fileName) {
+		try {
+			PrintWriter writer = new PrintWriter(fileName);
+			for (int i = 0; i < load.size(); i++){
+				writer.println(load.get(i));
+			}
+			writer.close();
+		} catch (Exception e) {
+			System.out.println("could not create a file with the name " + fileName);
+		}
 	}
 }
